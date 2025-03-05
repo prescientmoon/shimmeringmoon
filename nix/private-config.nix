16fdc89cf39d1e4594b92a8e51bf383ffe925f0f
prescientmoon <git@moonythm.dev>
2025-03-05 14:38:37 +0100

    Automatically merge content bundles in bash script

--- nix/private-config.nix
+++ nix/private-config.nix
@@ -2,27 +2,29 @@
   debundled-void,
   runCommand,
 }:
-let
-  jacketVersion = "6.2.3";
-  songlistVersion = "6.2.3.12";
-in
 runCommand "shimmering-private-config" { } ''
   mkdir $out
   mkdir $out/jackets
 
-  source=${debundled-void}/${jacketVersion}/songs
-  for dir in $source/*; do
-    out_dir=$(basename $dir)
-    out_dir=''${out_dir#dl_}
-    if [ -d $dir ] && [ $out_dir != "pack" ]; then
-      mkdir $out/jackets/$out_dir
+  for source in ${debundled-void}/*/songs; do
+    for dir in $source/*; do
+      out_dir=$(basename $dir)
+      out_dir=''${out_dir#dl_}
+      if [ -d $dir ] && [ $out_dir != "pack" ]; then
+        jacket_dir=$out/jackets/$out_dir
+        rm -rf $jacket_dir
+        mkdir $jacket_dir
 
-      for file in $dir/*_256.jpg; do
-        filename=$(basename $file)
-        cp $file $out/jackets/$out_dir/$filename
-      done
+        for file in $dir/*_256.jpg; do
+          filename=$(basename $file)
+          cp $file $out/jackets/$out_dir/$filename
+        done
+      fi
+    done
+
+    if [ -f $out ]; then
+      rm -rf $out/songlist.json
+      cp $source/songlist $out/songlist.json
     fi
   done
-
-  cp ${debundled-void}/${songlistVersion}/songs/songlist $out/songlist.json
 ''
