5eba70f8c7fc1400cc056e823b542670ad889a3d
prescientmoon <git@moonythm.dev>
2025-07-28 02:36:49 +0300

    Bump PTT data

--- nix/cc-data.nix
+++ nix/cc-data.nix
@@ -2,6 +2,6 @@
 pkgs.fetchFromGitHub {
   owner = "OllyDoge";
   repo = "ASCPSongConstant";
-  rev = "f99de5d1b9b1e82f24a284d97e986d7bb4d0fd69";
-  sha256 = "0vvl0852la65xkfxh4amrnh81m08v9aknlp73ifh7llxd1k6qb9q";
+  rev = "014ccb499ef80775fd0e72b78467a073b3be0763";
+  sha256 = "0cafavxpzyxpch7ar9q9r13nc44w85vil9l102m021l781dk6p3x";
 }
